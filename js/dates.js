b9d9753924bce047f6552d667c6f239f4892fd6c
lan21005 <lan21005@byui.edu>
2022-04-25 16:18:43 -0400

    updates

--- js/dates.js
+++ js/dates.js
@@ -3,12 +3,3 @@ lastmod.textContent = `Last Updated: ${document.lastModified}`;
 
 const year = document.querySelector("#year")
 year.innerHTML = new Date().getFullYear();
-
-
-//change profile picture on click
-function changeImage(element) {
-    var right = "images/profile.jpg";
-    var left = "images/easter_egg.jpg";
-    element.src = element.bln ? right : left;
-    element.bln = !element.bln;
-}
