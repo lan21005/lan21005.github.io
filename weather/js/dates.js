ae3a0854a34fcbe64ada657454df92a9a482f276
lan21005 <lan21005@byui.edu>
2022-04-26 23:55:43 -0400

    Preston initial upload

--- weather/js/dates.js
+++ weather/js/dates.js
@@ -1,5 +1,17 @@
+// Dates //
 const lastmod = document.querySelector('#lastmod')
 lastmod.textContent = `Last Updated: ${document.lastModified}`;
 
 const year = document.querySelector("#year")
 year.innerHTML = new Date().getFullYear();
+
+// Hamburger Button //
+/*Button css*/
+function toggleMenu() {
+    console.log("It worked!");
+    document.querySelector("#primarynav").classList.toggle("open");
+    document.querySelector("#hamburgerbtn").classList.toggle("open");
+  }
+  
+  const x = document.querySelector("#hamburgerbtn");
+  x.onclick = toggleMenu;
