643f26ee5098fd4da80ccf570811a06363437fa7
lan21005 <lan21005@byui.edu>
2022-04-29 21:54:10 -0400

    updates on dates

--- weather/js/dates.js
+++ weather/js/dates.js
@@ -5,17 +5,8 @@ lastmod.textContent = `Last Updated: ${document.lastModified}`;
 const year = document.querySelector("#year")
 year.innerHTML = new Date().getFullYear();
 
-const current_date = new Date();
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const day = current_date.getDate();
-const month_num = current_date.getMonth();
-const day_num = current_date.getDay();
-const day_name = weekday[day_num];
-const month_name = month[month_num];
-const date = document.querySelector("#current_date");
-current_date.innerHTML = `${day_name}, ${month_name} ${day}`;
-
+const d = new Date();
+document.getElementById("current_date").innerHTML = d.toDateString();
 
 // Hamburger Button //
 /*Button css*/
