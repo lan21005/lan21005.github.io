cf7c1ff3e38d3c108729807b8b51618ca89299b9
lan21005 <lan21005@byui.edu>
2022-05-23 21:38:43 -0400

    lesson-11 initial upload

---
+++ lesson-11/js/dates.js
@@ -0,0 +1,39 @@
+// Dates //
+const lastmod = document.querySelector("#lastmod");
+lastmod.textContent = `Last Updated: ${document.lastModified}`;
+
+const year = document.querySelector("#year");
+year.innerHTML = new Date().getFullYear();
+
+const d = new Date();
+//Display date in long format Weekday, Month Day, Year
+const date = document.querySelector("#date");
+document.getElementById("current_date").innerHTML = d.toDateString();
+
+
+// Hamburger Button //
+/*Button css*/
+function toggleMenu() {
+  console.log("It worked!");
+  document.querySelector("#primarynav").classList.toggle("open");
+  document.querySelector("#hamburgerbtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerbtn");
+x.onclick = toggleMenu;
+
+//Friday Banner/
+const banner = document.querySelector("#banner");
+const banner_day = new Date();
+const day = banner_day.getDay();
+if (day == 5) {
+  banner.innerHTML =
+    "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+} else {
+  banner.style.display = "none";
+}
+
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
