2b64cc38fee30f50729327d6e16f4402e4a85b19
lan21005 <lan21005@byui.edu>
2022-05-26 23:36:35 -0400

    initial js updates

---
+++ scoots/js/scripts.js
@@ -0,0 +1,22 @@
+// Dates //
+const lastmod = document.querySelector("#lastmod");
+lastmod.textContent = `Last Updated: ${document.lastModified}`;
+
+const year = document.querySelector("#year");
+year.innerHTML = new Date().getFullYear();
+
+const d = new Date();
+//Display date in long format Weekday, Month Day, Year
+const date = document.querySelector("#date");
+document.getElementById("current_date").innerHTML = d.toDateString();
+
+// Hamburger Button //
+/*Button css*/
+function toggleMenu() {
+  console.log("It worked!");
+  document.querySelector("#primarynav").classList.toggle("open");
+  document.querySelector("#hamburgerbtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerbtn");
+x.onclick = toggleMenu;
