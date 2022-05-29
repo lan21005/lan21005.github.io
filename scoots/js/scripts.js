94553a77526fdac8f09ceb019424522ea4ce1f01
lan21005 <lan21005@byui.edu>
2022-05-29 17:59:24 -0400

    updates

--- scoots/js/scripts.js
+++ scoots/js/scripts.js
@@ -21,7 +21,6 @@ function toggleMenu() {
 const x = document.querySelector("#hamburgerbtn");
 x.onclick = toggleMenu;
 
-
 function adjustRating(quantity) {
   document.getElementById("quantityvalue").innerHTML = quantity;
 }
@@ -30,35 +29,38 @@ var placeSearch, autocomplete;
 
 // List all address components (corresponds to form field IDs and Google address object)
 var componentForm = {
-  autocomplete: ['street_number', 'route'],
-  inputCity: 'locality',
-  inputState: 'administrative_area_level_1',
-  inputZip: 'postal_code',
-  inputCounty: 'administrative_area_level_2',
-  inputCountry: 'country'
+  autocomplete: ["street_number", "route"],
+  inputCity: "locality",
+  inputState: "administrative_area_level_1",
+  inputZip: "postal_code",
+  inputCounty: "administrative_area_level_2",
+  inputCountry: "country",
 };
 
 // Create autocomplete object based on the autocomplete ("street") field
 // Location type restricted to geocode
 function initAutocomplete() {
   autocomplete = new google.maps.places.Autocomplete(
-      /** @type {!HTMLInputElement} */ (document.getElementById('autocomplete')),
-      {type: ['geocode']});
+    /** @type {!HTMLInputElement} */ (document.getElementById("autocomplete")),
+    { type: ["geocode"] }
+  );
 
-  // Call fillInAddress when user selects an address from dropdown
-  autocomplete.addListener('place_changed', fillInAddress);
+  // Call fillInAddress when user selects an address from dropdown
+  autocomplete.addListener("place_changed", fillInAddress);
 }
 
 // Fill fields with values from Google Maps autocomplete object
 function fillInAddress() {
-
   // Get place data from autocomplete object
   var place = autocomplete.getPlace();
   console.log(place);
-  
+
   // Enable each field, then fill them with the corresponding value from the place object
   for (var component in componentForm) {
     document.getElementById(component).disabled = false;
-    document.getElementById(component).value = search(componentForm[component], place.address_components);
+    document.getElementById(component).value = search(
+      componentForm[component],
+      place.address_components
+    );
   }
 }
